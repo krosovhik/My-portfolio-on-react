b5526a55d4012076082b904feb8fc284c26f3375
Oleksandr Kovtun <krosovhiksanya228@gmail.com>
2023-02-01 22:47:01 +0100

    first commit

---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>Krakow, Poland</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Telegram / WhatsApp</h2>
+            <p>
+              <a href="tel:+79051234567">+48 999-999-999</a>
+            </p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>
+              <a href="mailto:webdev@protonmail.com">
+                krosovhiksanya228@gmail.com
+              </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Contacts;
